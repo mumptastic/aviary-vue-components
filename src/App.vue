e6239a29ced1ff6d349acee65d3c8cd77ef268d4
Michael Nichols <mumptastic@gmail.com>
2022-09-14 09:54:02 -0500

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,108 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <Header logo="https://api.library.tamu.edu/tl-components/1.12/assets/icons/tl/tamu-logo.svg" title="Something Else">
+  
+  <nav>
+      <ul>
+        <li><a href="//library.tamu.edu/about/hours">Hours</a></li>
+        <li class="menu">
+          <a href="//library.tamu.edu/about/index">Libraries</a>
+          <ul role="menu">
+            <li role="presentation">
+              <a href="//blcc.library.tamu.edu" title="Go to Business Library and Collaboration Commons website">Business Library & Collaboration Commons</a>
+              <a class="directions" href="//library.tamu.edu/about/directions/business-library" title="Parking Directions for Business Library and Collaboration Commons">Parking Directions</a>
+              <span class="phone">979-845-2111</span>
+            </li>
+            <li role="presentation">
+              <a href="//cushing.library.tamu.edu" title="Go to Cushing Library website">Cushing Memorial Library & Archives</a>
+              <a class="directions" href="//library.tamu.edu/about/directions/cushing-library" title="Parking Directions for Cushing Library">Parking Directions</a>
+              <span class="phone">979-845-1951</span>
+            </li>
+            <li role="presentation">
+              <a href="//msl.library.tamu.edu" title="Go to Medical Sciences Library website">Medical Sciences Library</a>
+              <a class="directions" href="//library.tamu.edu/about/directions/medical-sciences-library" title="Parking Directions for Medical Sciences Library">Parking Directions</a>
+              <span class="phone">979-845-7428</span>
+            </li>
+            <li role="presentation">
+              <a href="//psel.library.tamu.edu" title="Go to Policy Sciences and Economics Library website">Policy Sciences & Economics Library</a>
+              <a class="directions" href="//library.tamu.edu/about/directions/policy-sciences-economics-library" title="Parking Directions for Policy Sciences and Economics Library">Parking Directions</a>
+              <span class="phone">979-845-3731</span>
+            </li>
+            <li role="presentation">
+              <a href="//library.tamu.edu" title="Go to Evans Library website">Sterling C. Evans Library & Annex</a>
+              <a class="directions" href="//library.tamu.edu/about/directions/evans-library-annex" title="Parking Directions for Evans Library and Annex">Parking Directions</a>
+              <span class="phone">979-845-3731</span>
+            </li>
+          </ul>
+        </li>
+        <li class="menu">
+          <a href="//library.tamu.edu">Information for</a>
+          <ul role="menu">
+            <li role="presentation">
+              <a role="menuitem" href="//library.tamu.edu/faculty_info">Information for Faculty</a>
+            </li>
+            <li role="presentation">
+              <a role="menuitem" href="//library.tamu.edu/grad_info">Graduate Student Resources</a>
+            </li>
+            <li role="presentation">
+              <a role="menuitem" href="//library.tamu.edu/undergrad_info">Undergraduate Student Resources</a>
+            </li>
+            <li role="presentation">
+              <a role="menuitem" href="//library.tamu.edu/services/accessibility">Services for Individuals with Disabilities</a>
+            </li>
+            <li role="presentation">
+              <a role="menuitem" href="//tamu.libguides.com/c.php?g=949823">Students at Satellite Campuses</a>
+            </li>
+            <li role="presentation">
+              <a role="menuitem" href="//tamu.libguides.com/c.php?g=586101&amp;p=4048709">School Visitors</a>
+            </li>
+            <li role="presentation">
+              <a role="menuitem" href="//library.tamu.edu/about/other_patrons">Other Library Guests (Non-TAMU Affiliates)</a>
+            </li>
+          </ul>
+        </li>
+        <li><a href="//library.tamu.edu/mylibrary">MyLibrary</a></li>
+        <li class="menu">
+          <a href="//askus.library.tamu.edu">Help</a>
+          <ul role="menu">
+            <li role="presentation">
+              <a role="menuitem" href="//askus.library.tamu.edu">Chat with us</a>
+            </li>
+            <li role="presentation">
+              <a role="menuitem" href="//library.tamu.edu/about/phone">Call us</a>
+            </li>
+            <li role="presentation">
+              <a role="menuitem" href="sms:9792561091">Text us @ 979-256-1091</a>
+            </li>
+            <li role="presentation">
+              <a role="menuitem" href="//askus.library.tamu.edu/contact">Email us</a>
+            </li>
+            <li role="presentation">
+              <a role="menuitem" href="//askus.library.tamu.edu">FAQs</a>
+            </li>
+            <li role="presentation">
+              <a role="menuitem" href="//askus.library.tamu.edu/contact">Report a problem</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  
+ </Header>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './assets/style.scss'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+<a class="skip-to-content" href="#content">Skip to content</a>
+
+<header aria-label="header">
+  <div class="top-bar">
+    <div class="container">
+      <div class="logo">
+        <a :href="`${logoHref}`">
+            <span>
+                <img :src="`${logo}`" :alt="`${title}`"/>
+                <span class="mastTitle">{{mastTitle}}</span>
+            </span>
+        </a>
+      </div>
+      <div class="navigation">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+  <div class="masthead">
+    <div class="container">
+      <a :href="`${titleHref}`">
+        <h1>{{title}}</h1>
+      </a>
+    </div>
+  </div>
+</header>
+
+</template>
+
+<script>
+export default {
+  name: 'AviaryHeader',
+  props: {
+    logo: String,
+    logoHref: {
+        type: String,
+        default: '#'
+    },
+    mastTitle:{
+        type: String,
+        default: "Texas A&M University Libraries"
+    },
+    titleHref: {
+        type: String,
+        default: 'https://library.tamu.edu'
+    },
+    title: {
+        type: String,
+        default: 'Aviary'
+    },
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+        
+
+</style>
